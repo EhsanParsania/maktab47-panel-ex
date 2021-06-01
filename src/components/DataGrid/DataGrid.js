eb67aafc5b55fedc986d1b0a0dfe9ad6db3497f8
EhsanParsania <ehsan.parsa69@gmail.com>
2021-06-01 19:58:41 +0430

    add InputData to Modal

--- src/components/DataGrid/DataGrid.js
+++ src/components/DataGrid/DataGrid.js
@@ -1,8 +1,8 @@
 import { Component } from "react"
-import {Button} from "../index"
-import {Modal} from "../index"
+import { Button } from "../index"
+import { Modal } from "../index"
 import style from "./DataGrid.module.css"
-
+import { InputFile } from '../index'
 class Datagrid extends Component {
 
     state = {
@@ -10,59 +10,62 @@ class Datagrid extends Component {
     }
 
     activeModalStyle = {
-        opacity:1,
-        top:'100px',
-        width:'550px',
-        height:'550px'
+        opacity: 1,
+        top: '100px',
+        width: '550px',
+        height: '550px'
     }
 
     generalModalStyle = {
-        borderRadius:'12px'
+        borderRadius: '12px',
+        padding:'0 20px'
     }
 
     constructor(props) {
         super(props)
     }
 
-    modalDisplayHandler = ({target}) => {
-        this.setState({ showModel : !this.state.showModel })
+    modalDisplayHandler = ({ target }) => {
+        this.setState({ showModel: !this.state.showModel })
     }
 
     render() {
-        const {data, title} = this.props
-        const fields = Object.keys(data[0]).filter(field => field!=="id")
+        const { data, title } = this.props
+        const fields = Object.keys(data[0]).filter(field => field !== "id")
         return (
             <div id="datagrid">
-                <Modal activeStyle = { this.state.showModel ? this.activeModalStyle : {} } generalStyle={this.generalModalStyle}>
-                    <Button click={this.modalDisplayHandler} text="close" cssClass={style.closeButton}/>
+                <Modal activeStyle={this.state.showModel ? this.activeModalStyle : {}} generalStyle={this.generalModalStyle}>
+                 
+                    <Button click={this.modalDisplayHandler} text="✖" cssClass={style.closeButton} />
+                    <InputFile />
                 </Modal>
                 <div className={style.datagridContainer}>
                     <div className={style.datagridHeader}>
                         <h2>{title}</h2>
                         <div>
                             <Button text="Share" cssClass={style.shareButton} />
-                            <Button text="Export" cssClass={style.exportButton}/>
-                            <Button text="This Week" cssClass={style.weekButton} cssStyle={{marginLeft:"20px"}} />
-                            <Button text="Add" cssClass={style.addButton} click={this.modalDisplayHandler}/>
+                            <Button text="Export" cssClass={style.exportButton} />
+                            <Button text="This Week" cssClass={style.weekButton} cssStyle={{ marginLeft: "20px" }} />
+                            <Button text="Add" cssClass={style.addButton} click={this.modalDisplayHandler} />
                         </div>
                     </div>
                     <table className={style.datagridTable}>
                         <thead>
-                            <tr style={{backgroundColor:"#eee"}}>
+                            <tr style={{ backgroundColor: "#eee" }}>
                                 <th>#</th>
                                 {fields.map((field, fieldIndex) => <th key={fieldIndex}>{field}</th>)}
                             </tr>
                         </thead>
                         <tbody>
-                            {data.map( (item, dataIndex) => {
+                            {data.map((item, dataIndex) => {
                                 return (
-                                    <tr key ={dataIndex} style={{backgroundColor: dataIndex%2===0 ? "#fff" : "#eee"}} >
+                                    <tr key={dataIndex} style={{ backgroundColor: dataIndex % 2 === 0 ? "#fff" : "#eee" }} >
                                         <td>{item['id']}</td>
-                                        {fields.map( (field, fieldIndex)=><td key={fieldIndex}>{item[field]}</td>)}
+                                        {fields.map((field, fieldIndex) => <td key={fieldIndex}>{item[field]}</td>)}
                                     </tr>
                                 )
                             })}
-                            
+
                         </tbody>
                     </table>
                 </div>
@@ -72,4 +75,4 @@ class Datagrid extends Component {
     }
 }
 
-export {Datagrid}
+export { Datagrid }
