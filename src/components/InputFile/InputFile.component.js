fd8a8919bf6473f347bc689d0729890c1dc8c1e5
EhsanParsania <ehsan.parsa69@gmail.com>
2021-06-02 11:05:17 +0430

    change choose file to only image

--- src/components/InputFile/InputFile.component.js
+++ src/components/InputFile/InputFile.component.js
@@ -6,6 +6,8 @@ class InputFile extends Component {
 
   }
 
+
+  //xhr request to post image with form data ( multipart/form-data )
   uploadFile() {
     return new Promise(function (resolve, reject) {
       const image = document.getElementById('input').files[0]
@@ -20,14 +22,11 @@ class InputFile extends Component {
             if (image) {
               resolve(true)
             }
-
           }
           else {
-            reject('file not send')
+            reject('file not sent')
           }
-
         }
-
       });
       xhr.open("POST", "http://localhost:3001/upload");
       xhr.send(data);
@@ -42,11 +41,10 @@ class InputFile extends Component {
     const fileName = event.target.files.item(0).name
     await this.setState({ fileName: fileName })
 
-    // add image displayer:
+    // show image box:
     let reader = new FileReader()
     reader.onload = function () {
       let output = document.getElementById('output')
-
       output.src = reader.result
       output.style.display = 'flex'
     }
@@ -59,9 +57,8 @@ class InputFile extends Component {
   submitForm = async (e) => {
     e.preventDefault();
 
-    // async ajax post
+    // post file and get boolean result
     const isUpload = await this.uploadFile()
-    console.log(isUpload)
 
     // remove modal
     this.props.closeModal(isUpload)
@@ -75,7 +72,7 @@ class InputFile extends Component {
       <form action='http://localhost:3001/upload' method='POST' encType='multipart/form-data' onSubmit={this.submitForm} className={style.input_file_container}>
         <label className={style.input_file_label}>
           <span className={style.file_name} >{this.state.fileName}</span>
-          <input id='input' type="file" name='image' onChange={this.handleChange} className={style.input_file} />
+          <input id='input' type="file" name='image' onChange={this.handleChange} className={style.input_file} accept='image/*' />
           <i className={style.upload_button}>Upload File</i>
         </label>
         <img src="" className={style.image_container} id="output" alt="pic..."></img>
