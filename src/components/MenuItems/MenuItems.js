1fcb7cf48d47fc37a731f94cfa2db12066c7fbb5
EhsanParsania <ehsan.parsa69@gmail.com>
2021-06-08 11:08:16 +0430

    add on hash change for back & frwd button

--- src/components/MenuItems/MenuItems.js
+++ src/components/MenuItems/MenuItems.js
@@ -1,16 +1,26 @@
-import {Component} from 'react'
+import { Component } from 'react'
 import style from "./MenuItems.module.css"
-import {getData} from "../../model/AjaxRequests"
+import { getData } from "../../model/AjaxRequests"
+
+
 
 class MenuItems extends Component {
 
-    handleClick = async (dataName)=>{
+    handleClick = async (dataName) => {
         const data = await getData(dataName)
-        console.log(data)
         const fetchedData = JSON.parse(data)
-        const length = fetchedData.length
-        this.props.setItemsStart(0,1)
+        this.props.setItemsStart(0, 1)
         this.props.dataSetter(fetchedData, dataName)
+    }
+
+    shouldComponentUpdate() {
+        onhashchange = async () => {
+            const hash = window.location.hash.slice(1)
+            console.log(hash)
+            await this.handleClick(hash)
+        }
+
+        return true
     }
 
     render() {
@@ -18,23 +28,23 @@ class MenuItems extends Component {
             <div className={style.sideNav} id="menuitems">
                 <div className={style.sideDiv}>
                     <span className={style.sideSpan}>
-                    <img className={style.sideImg} src="https://aux.iconspalace.com/uploads/dashboard-icon-256-1241939503.png"/></span>
-                    <a className={style.sideLink} href="#dashboard" onClick={()=>this.handleClick('orders')}>Dashboard</a>
+                        <img className={style.sideImg} src="https://aux.iconspalace.com/uploads/dashboard-icon-256-1241939503.png" /></span>
+                    <a className={style.sideLink} href="#customers" >Dashboard</a>
                 </div>
                 <div className={style.sideDiv}>
                     <span className={style.sideSpan}>
-                    <img className={style.sideImg} src="https://img.icons8.com/pastel-glyph/2x/purchase-order.png" /></span>
-                    <a className={style.sideLink} href="#orders" onClick={()=>this.handleClick('orders')}>Orders</a>
+                        <img className={style.sideImg} src="https://img.icons8.com/pastel-glyph/2x/purchase-order.png" /></span>
+                    <a className={style.sideLink} href="#orders" >Orders</a>
                 </div>
                 <div className={style.sideDiv}>
                     <span className={style.sideSpan}>
-                    <img className={style.sideImg} src="https://cdn.iconscout.com/icon/free/png-256/customers-11-866159.png" /></span>
-                    <a className={style.sideLink} href="#products" onClick={()=>this.handleClick('products')}>Products</a>
+                        <img className={style.sideImg} src="https://cdn.iconscout.com/icon/free/png-256/customers-11-866159.png" /></span>
+                    <a className={style.sideLink} href="#products" >Products</a>
                 </div>
                 <div className={style.sideDiv}>
                     <span className={style.sideSpan}>
-                    <img className={style.sideImg} src="https://cdn2.iconfinder.com/data/icons/e-commerce-line-4-1/1024/open_box4-512.png" /></span>
-                    <a className={style.sideLink} href="#customers" onClick={()=>this.handleClick('customers')}>Customers</a>
+                        <img className={style.sideImg} src="https://cdn2.iconfinder.com/data/icons/e-commerce-line-4-1/1024/open_box4-512.png" /></span>
+                    <a className={style.sideLink} href="#customers" >Customers</a>
                 </div>
             </div>
         )
